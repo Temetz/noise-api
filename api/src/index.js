c47dc9a168ed0cb04a87dc434c4c5a47364a2e10
Temetz <teemy@t-palvelut.net>
2015-05-21 13:43:05 +0000

    bugfixings

--- api/src/index.js
+++ api/src/index.js
@@ -8,17 +8,17 @@ var PORT = 5300;
 var app = express();
 app.use(require('body-parser')());
 app.put('/limit', function (req, res) {
-    shell.exec('sudo tc qdisc change dev lo root netem loss '+
+    var status = shell.exec('sudo tc qdisc change dev lo root netem loss '+
                             req.body.loss+'% delay '+req.body.delay+
                             'ms', {silent:true}).output;
 
-    var status = shell.exec('sudo tc qdisc show', {silent:true}).output;
+    var statusss = shell.exec('sudo tc qdisc show', {silent:true}).output;
     res.send('Status:' + status);
 });
 
 app.get('/', function (req, res) {
     console.log('shelsl');
-    res.send('Hello! This is root.');
+    res.send('Hello! This is root. Again');
 });
 
 app.listen(PORT);
