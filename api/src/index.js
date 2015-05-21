b900802a86d8c45ece54496b4039410d87365f5c
Teemu Kontio <teemu@t-palvelut.net>
2015-05-21 14:56:43 +0300

    Added dummy shell /node

--- api/src/index.js
+++ api/src/index.js
@@ -1,12 +1,18 @@
 var express = require('express');
+var shell = require('shelljs');
 
 // Constants
 var PORT = 5300;
 
 // App
 var app = express();
+app.get('/node', function (req, res) {
+    var version = exec('node --version', {silent:true}).output;
+    res.send(version);
+});
+
 app.get('/', function (req, res) {
-  res.send('Hello world\n');
+    res.send('Hello! This is root');
 });
 
 app.listen(PORT);
