a3f2597e728bd7bce7e752d7118193b23921a3aa
Teemu Kontio <teemu@t-palvelut.net>
2015-05-22 13:53:00 +0300

    Added api gui

--- api/src/index.js
+++ api/src/index.js
@@ -1,12 +1,24 @@
-var express = require('express');
-var shell = require('shelljs');
+
+var express         = require('express');
+var shell           = require('shelljs');
+var path            = require("path");
+var bodyParser      = require('body-parser');
+var methodOverride  = require('method-override')
 
 // Constants
 var PORT = 5300;
 
 // App
 var app = express();
-app.use(require('body-parser')());
+
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
+app.use(methodOverride('_method'));
+require('jade-method-override').express(app)
+
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
+
 app.put('/limit', function (req, res) {
     shell.exec('sudo tc qdisc change dev lo root netem loss '+
                             req.body.loss+'% delay '+req.body.delay+
@@ -16,7 +28,11 @@ app.put('/limit', function (req, res) {
 });
 
 app.get('/', function (req, res) {
-    res.send('Hello! This is root. Again');
+    res.render('welcome', {host:req.headers['host']});
+});
+
+app.get('/configure', function (req, res) {
+    res.render('configure');
 });
 
 app.listen(PORT);
