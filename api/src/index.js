8949acb6fd1474b58b96dd5ebeacc3297f0f184f
Teemu Kontio <teemu@t-palvelut.net>
2015-05-25 11:13:54 +0300

    Changed paths Added target

--- api/src/index.js
+++ api/src/index.js
@@ -11,7 +11,7 @@ var PORT = 5300;
 var app = express();
 
 app.use(bodyParser.json());
-app.use(express.static(__dirname + '/public'));
+app.use('/static', express.static(__dirname + '/public'));
 
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
@@ -30,12 +30,27 @@ app.put('/limit', function (req, res) {
     res.json(status);
 });
 
+app.put('/target', function (req, res) {
+    var command = "sudo sed -i '35s/.*/server target " + req.body.httptarget + ":80/' /etc/haproxy/haproxy.cfg";
+    shell.exec(command, {silent:true});
+    var status = shell.exec('sudo service haproxy restart', {silent:true}).output;
+    res.json(status);
+});
+
 app.get('/', function (req, res) {
+    res.redirect('/gui/welcome');
+});
+
+app.get('/gui/welcome', function (req, res) {
     res.render('welcome', {host:req.headers['host']});
 });
 
-app.get('/configure', function (req, res) {
+app.get('/gui/configure', function (req, res) {
     res.render('configure');
+});
+
+app.get('/gui/target', function (req, res) {
+    res.render('target');
 });
 
 app.listen(PORT);
