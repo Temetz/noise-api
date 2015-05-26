58741128198509ddd21ac0448004e44a0f14f613
Teemu Kontio <teemu@t-palvelut.net>
2015-05-26 10:44:34 +0300

    Multi proxy support

--- build_remove_run.sh
+++ build_remove_run.sh
@@ -1,4 +1,4 @@
 sudo docker stop noise-api
 sudo docker rm noise-api
 sudo docker build -t temetz/noise-api .
-sudo docker run -i -t -d -p 80:80 -p 443:443 -p 5300:5300 -e "APIKEY=$1" --privileged --name noise-api temetz/noise-api
+sudo docker run -i -t -d -p $1:80 -p $2:5300 -e "APIKEY=$3" --privileged --name noise-api temetz/noise-api
