709e100f4891fc2834596c5efc34a115edfeb537
Teemu Kontio <teemu@t-palvelut.net>
2015-05-28 21:49:25 +0300

    This in now only a API

--- Dockerfile
+++ Dockerfile
@@ -16,7 +16,6 @@ COPY /api /api
 RUN cd /api/src;npm install
 
 EXPOSE 80
-EXPOSE 443
 EXPOSE 5300
 
 CMD sudo service haproxy restart && sudo tc qdisc add dev lo root netem loss 0% delay 0ms && node /api/src/index.js
--- api/src/index.js
+++ api/src/index.js
@@ -1,8 +1,15 @@
 var express         = require('express');
 var shell           = require('shelljs');
-var path            = require("path");
 var bodyParser      = require('body-parser');
 var cookieParser    = require('cookie-parser');
+
+//CORS middleware
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+    next();
+}
 
 // Constants
 var PORT = 5300;
@@ -10,39 +17,17 @@ var PORT = 5300;
 // App
 var app = express();
 
-// Authentication
-app.locals.state = {
-    authenticated: false
-}
-
 app.use(bodyParser.json());
 app.use(cookieParser('verysecurepassword'));
-app.use('/static', express.static(__dirname + '/public'));
-
-app.set('view engine', 'jade');
-app.set('views', __dirname + '/views');
-
+app.use(allowCrossDomain);
 app.all('*', checkUser);
 
 function checkUser(req, res, next) {
-  var _ = require('underscore')
-      , nonSecurePaths = ['/', '/gui/login', '/login'];
-
-  if ( _.contains(nonSecurePaths, req.path) ){
-    if(app.locals.state.authenticated === true){
-        app.locals.state.authenticated = false;
-        return next();
-    }
-    return next();
-  }
-
   if(req.cookies.apikey == process.env.APIKEY){
-        app.locals.state.authenticated = true;
         next();
     }
     else {
-        app.locals.state.authenticated = false;
-        res.redirect('/gui/login');
+        res.status(401).json({ error: 'Unauthorized' });
     }
 }
 
@@ -53,11 +38,9 @@ app.put('/limit', function (req, res) {
     command += req.body.delayvariance + 'ms ';
     if(req.body.jitter === 1){
         command += 'distribution ' + req.body.distribution;
-
     }
     shell.exec(command, {silent:true});
     var status = shell.exec('sudo tc qdisc show', {silent:true}).output;
-    res.json(status);
 });
 
 app.put('/target', function (req, res) {
@@ -67,46 +50,9 @@ app.put('/target', function (req, res) {
     res.json(status);
 });
 
-app.post('/login', function (req, res) {
-    if(req.body.apikey == process.env.APIKEY)
-    {
-        res.cookie('apikey', req.body.apikey);
-        res.json("Login success");
-        return;
-    }
-    res.clearCookie('apikey', '')
-    res.status(401);
-    res.json("Invalid login");
-});
-
-app.post('/logout', function (req, res) {
-    res.clearCookie('apikey', '')
-    res.json("You have logged out");
-});
-
 app.get('/', function (req, res) {
-    res.redirect('/gui/welcome');
-});
-
-app.get('/gui/welcome', function (req, res) {
-    res.render('welcome', {host:req.headers['host']});
-});
-
-app.get('/gui/configure', function (req, res) {
-    res.render('configure');
-});
-
-app.get('/gui/target', function (req, res) {
-    res.render('target');
-});
-
-app.get('/gui/logout', function (req, res) {
-        res.render('logout');
-});
-
-app.get('/gui/login', function (req, res) {
-        res.render('login');
-});
+    res.redirect(301, 'http://management.noise.n4sjamk.org');
+}
 
 app.listen(PORT);
-console.log('Running on http://localhost:' + PORT);
+console.log('Running on port:' + PORT);
